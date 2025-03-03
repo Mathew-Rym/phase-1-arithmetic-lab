f322bce84471dc3fe1370e9fe1bec491ad91ada9
mathew rym <rymmathew@gmail.com>
2025-03-04 01:34:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 // Write your code here
-// Define num1 and num2 for multiplication
+// 31 * 2 = 62
 const num1 = 31;
 const num2 = 2;
-const multiply = num1 * num2; // 31 * 2 = 62
+const multiply = num1 * num2; 
 
-// Generate a random integer greater than 0
-const random = Math.floor(Math.random() * 100) + 1; // Ensures the number is > 0
+// random integer ensures the number is > 0
+const random = Math.floor(Math.random() * 8) + 1; 
 
-// Define num3 and num4 for modulus operation
-const num3 = 28;
-const num4 = 6;
-const mod = num3 % num4; // 28 % 6 = 4
+// 32 % 7 = 4
+const num3 = 32;
+const num4 = 7;
+const mod = num3 % num4; 
 
-// Find the max number in a set
-const max = Math.max(10, 5, 20, 15); // Returns 20
+// max number in a set Returns 20
+const max = Math.max(1, 8, 20, 17); 
